8973256fcce3db5c1af970504c7e29fd89c27185
Mike Bostone <mikebostone@gmail.com>
2016-08-28 16:52:58 -0500

    clean up includes

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,5 +5,4 @@
 	</footer>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-	<!--<script src="scripts/libs.js"></script>-->
 	<script src="scripts/core.js"></script>
--- includes/head.inc.php
+++ includes/head.inc.php
@@ -5,7 +5,6 @@
 
 	<title>Neptune's Window Aquarium Services</title>
 	<!--<link rel="icon" href="images/favicon.png" type="image/x-icon">-->
-	<!--<link href="css/libs.css" rel="stylesheet" type="text/css">-->
 	<link href="css/core.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat%7COpen+Sans:400,600" rel="stylesheet">
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
