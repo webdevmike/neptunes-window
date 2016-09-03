35efd61f8471957f6c579ad0e8f2205391efff7e
Mike Bostone <mikebostone@gmail.com>
2016-09-03 14:54:19 -0500

    inspiration page desktop built

---
+++ inspiration.php
@@ -0,0 +1,97 @@
+<?php $nav = 'inspiration'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('includes/head.inc.php'); ?>
+</head>
+
+<body>
+<?php include('includes/header.inc.php'); ?>
+
+	<main class="inspiration">
+		<div class="content-container">
+			
+			<h1>Inspiration</h1>
+
+			<div class="intro">Often an idea for a project comes from inspiration in everyday life...A childhood memory, a trip to a zoo or public aquarium, or a picture seen in a magazine. Sometimes it’s difficult to visualize the type of aquarium system that would be the best fit for you and your space without knowing what options are available to you. Below are some of the biotopes that we specialize in creating.  If something catches your eye, <a href="tel:1-920-915-7302">call us</a> and we can discuss it in further detail.</div>
+
+			<ul class="top-inspiration">
+				<li>
+					<div class="image">
+						<img src="images/compressed/inspiration-pic-1.jpg" alt="Mixed Coral Reef">
+					</div>
+					<div class="description">
+						<h2>Mixed Coral Reef</h2>
+						<p>This type of system will have a variety of different types of coral, fish and invertebrates all comingling in harmony.</p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<img src="images/compressed/inspiration-pic-2.jpg" alt="Stony Coral Reef">
+					</div>
+					<div class="description">
+						<h2>Stony Coral Reef</h2>
+						<p>This system focuses primarily on small polyped stony (sps) corals, smaller sized fish, and is considered by many, the pinnacle of reef keeping.</p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<img src="images/compressed/inspiration-pic-3.jpg" alt="Marine Fish Only">
+					</div>
+					<div class="description">
+						<h2>Marine Fish Only</h2>
+						<p>This option is preferred by folks who like large or aggressive fish that may not be suitable in a mixed reef environment.  Often these set-ups include fake coral decorations or inserts that need to be removed for cleaning on a regular basis.</p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<img src="images/compressed/inspiration-pic-4.jpg" alt="Planted Freshwater">
+					</div>
+					<div class="description">
+						<h2>Planted Freshwater</h2>
+						<p>This is an advanced freshwater setup that can rival marine reefs with their plants in striking color variations.</p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<img src="images/compressed/inspiration-pic-5.jpg" alt="African Cichlids">
+					</div>
+					<div class="description">
+						<h2>African Cichlids</h2>
+						<p>Large numbers of these can be housed together.  They are hardy fish that can get by with minimal maintenance, and come in a large assortment of colors.  It’s very interesting to watch the social hierarchy emerge.</p>
+					</div>
+				</li>
+				<li>
+					<div class="image">
+						<img src="images/compressed/inspiration-pic-6.jpg" alt="Koi Ponds">
+					</div>
+					<div class="description">
+						<h2>Koi Ponds</h2>
+						<p>These are ancient fish that have been bred by Japanese fish keepers for thousands of years.  In time they will eat from your hand like a dog.  They are often incorporated in zen-like backyard ponds.</p>
+					</div>
+				</li>
+			</ul>
+
+			<div class="bottom-inspiration">
+
+				<div class="description">
+					<h2>Species Aquarium</h2>
+					<p>Many animals have a very specific set of requirements that need to be met for them to thrive.  Jellyfish and seahorses are a good example of tank inhabitants that can do well if a system is designed keeping those needs in mind.</p>
+				</div>
+
+				<div class="image one">
+					<img src="images/compressed/inspiration-pic-8.jpg" alt="Species Aquarium">
+				</div>
+
+				<div class="image two">
+					<img src="images/compressed/inspiration-pic-7.jpg" alt="Species Aquarium">
+				</div>
+
+			</div>
+
+		</div><!-- .content-container -->
+	</main>
+
+<?php include('includes/footer.inc.php'); ?>
+</body>
+</html>
